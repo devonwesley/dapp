19bcee6a00f6cebcf28c39c9c612bb8eaa9b16f0
Devon T. Wesley <dwesley89@gmail.com>
2019-05-02 19:08:26 -0700

    fix: changes to flow

--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ import 'isomorphic-fetch';
 
 import ServiceRunner from 'jade-service-runner-package';
 import MGethService from 'mgeth-service';
-const client = new ServiceRunner({transport: { type: 'browser', port: 8002}});
-const mgeth = new MGethService({transport: { type: 'browser', port: 8545}})
+let mgeth;
+async function init() {
+  const client = new ServiceRunner({transport: { type: 'browser', port: 8002}});
+  const mgethConfig = await client.startService('mgeth', 'mainnet');
+  mgeth = new MGethService({transport: { type: 'browser', ...mgethConfig}});
+  client.installService('mgeth', '1').then((x) => console.log(x));
+}
 
-client.installService('mgeth', '1').then((x) => console.log(x));
+init();
 
 const submitAccount = document.getElementById('submitAccount');
 const accountAddress = document.getElementById('accountAddress');
@@ -14,6 +19,6 @@ const accountAddress = document.getElementById('accountAddress');
 submitAccount.addEventListener('click', function(event) {
   event.preventDefault();
   mgeth.eth_getBalance(accountAddress.value, '0x0')
-    .then((x) => console.log('balance: ' + parseInt(x)));
+    .then((x) => console.log('balance: ' + parseInt(x)))
     .catch(error => console.log('ERROR :: ==>', error));
 });
