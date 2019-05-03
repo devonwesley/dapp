34bb1c39be69c0e6bc73ea4a6795590bc8a93763
Zane Starr <zcstarr@gmail.com>
2019-05-02 17:13:30 -0700

    feat: add mgeth support to browser for demo app

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ require('es6-promise').polyfill();
 import 'isomorphic-fetch';
 
 import ServiceRunner from 'jade-service-runner-package';
+import MGethService from 'mgeth-service';
 const client =new ServiceRunner({transport: { type: 'browser', port: 8002}});
+const mgeth = new MGethService({transport: { type: 'browser', port: 8545}})
 client.installService('mgeth', '1').then((x) => console.log(x));
+mgeth.eth_getBalance('0x0deadbeef','0x0').then((x)=>console.log('balance:'+ x));
 
 const submitAccount = document.getElementById('submitAccount');
 const accountAddress = document.getElementById('accountAddress');
